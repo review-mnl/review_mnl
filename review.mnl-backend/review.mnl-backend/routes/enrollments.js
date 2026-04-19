bea89a821545efe8767dd6be2db275be2f8c0306
Zaki <mzboongaling@tip.edu.ph>
2026-04-20 00:21:11 +0800

    .

--- review.mnl-backend/review.mnl-backend/routes/enrollments.js
+++ review.mnl-backend/review.mnl-backend/routes/enrollments.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const { protect, centerOnly } = require('../middleware/auth');
-const { getCenterEnrollmentsByCenterId, deleteEnrollment } = require('../controllers/enrollmentController');
+const { getCenterEnrollmentsByCenterId } = require('../controllers/enrollmentController');
 
 router.get('/center/:centerId', protect, centerOnly, getCenterEnrollmentsByCenterId);
-router.delete('/:id', protect, deleteEnrollment);
 
 module.exports = router;
